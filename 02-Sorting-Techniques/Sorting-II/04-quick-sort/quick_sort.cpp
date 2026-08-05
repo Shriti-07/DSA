f096abe83f9db13fb8871e612e795f4169a6033b
Shriti-07 <shriti.tech07@gmail.com>
2026-08-05 11:31:48 +0530

    Solved Quick Sort Question

--- 02-Sorting-Techniques/Sorting-II/04-quick-sort/quick_sort.cpp
+++ 02-Sorting-Techniques/Sorting-II/04-quick-sort/quick_sort.cpp
@@ -1,19 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-class Solution {
-public:
-    vector<int> quickSort(vector<int>& nums) {
-        // TODO: write your solution here
-
+int divide(vector<int> &arr,int low,int high){
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i]<=pivot && i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot && j>=low+1){
+            j--;
+        }
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
     }
-};
+    swap(arr[low],arr[j]);
+    return j;
+    
+}
+void quickSort(vector<int> &arr,int low,int high){
+    if(low<high){
+        int pivot=divide(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+    }
+}
 
 int main() {
-    vector<int> nums = {5, 1, 4, 2, 8};
-    Solution sol;
-    vector<int> result = sol.quickSort(nums);
-    for (int x : result) cout << x << " ";
-    cout << endl;
+    vector<int> arr={1,2,2,8,5,21,67,9,33};
+    int n=arr.size();
+    quickSort(arr,0,n-1);
+    for(auto i: arr){
+        cout<<i<<" ";
+    }
     return 0;
 }
