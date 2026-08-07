f94ebdeb87b5643a76c5ebb58025605f83bd8631
Shriti-07 <shriti.tech07@gmail.com>
2026-08-07 22:33:29 +0530

    Added solution to the recursive bubble sort question

--- 02-Sorting-Techniques/Sorting-II/02-recursive-bubble-sort/recursive_bubble_sort.cpp
+++ 02-Sorting-Techniques/Sorting-II/02-recursive-bubble-sort/recursive_bubble_sort.cpp
@@ -3,17 +3,25 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> recursiveBubbleSort(vector<int>& nums) {
-        // TODO: write your solution here
-
+    void recursiveBubbleSort(vector<int>& nums,int n) {
+        if(n<=1){
+            return;
+        }
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                swap(nums[i],nums[i+1]);
+            }
+        }
+        recursiveBubbleSort(nums,n-1);
     }
 };
 
 int main() {
     vector<int> nums = {5, 1, 4, 2, 8};
+    int n=nums.size();
     Solution sol;
-    vector<int> result = sol.recursiveBubbleSort(nums);
-    for (int x : result) cout << x << " ";
+    sol.recursiveBubbleSort(nums,n);
+    for (int x : nums) cout << x << " ";
     cout << endl;
     return 0;
 }
