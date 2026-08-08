855db4a4da20068d8e69e446db0516a8f511d0bc
Shriti-07 <shriti.tech07@gmail.com>
2026-08-08 11:26:04 +0530

    Added Soltuion to insertion sort question

--- 02-Sorting-Techniques/Sorting-II/03-recursive-insertion-sort/recursive_insertion_sort.cpp
+++ 02-Sorting-Techniques/Sorting-II/03-recursive-insertion-sort/recursive_insertion_sort.cpp
@@ -3,17 +3,27 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> recursiveInsertionSort(vector<int>& nums) {
-        // TODO: write your solution here
-
+    void recursiveInsertionSort(vector<int>& nums,int n) {
+        if(n<=1){
+            return;
+        }
+        recursiveInsertionSort(nums,n-1);
+        int end=nums[n-1];
+        int j=n-2;
+        while(j>=0 && nums[j]>end){
+            nums[j+1]=nums[j];
+            j--;
+        }
+        nums[j+1]=end;
     }
 };
 
 int main() {
     vector<int> nums = {5, 1, 4, 2, 8};
+    int n=nums.size();
     Solution sol;
-    vector<int> result = sol.recursiveInsertionSort(nums);
-    for (int x : result) cout << x << " ";
+    sol.recursiveInsertionSort(nums,n);
+    for (int x : nums) cout << x << " ";
     cout << endl;
     return 0;
 }
