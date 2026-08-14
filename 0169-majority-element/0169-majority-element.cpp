5f393ac3e5d64cbf3840c80fc77619a374639578
Shriti-07 <shriti.tech07@gmail.com>
2026-08-14 20:37:39 +0530

    Time: 2 ms (31.53%), Space: 28.2 MB (43.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> count;
+        for(auto i :nums){
+            count[i]++;
+        }
+        /* int maxNumCount=0;
+        int maxNum=0; */
+        for(auto it:count){
+            if(it.second>n/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+/* class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]==nums[i]){
+                    count++;
+                }
+            }
+            if(count>=(nums.size()/2)){
+                return nums[i];
+            }
+            count=0;
+        }
+        return -1;
+    }
+};  */
