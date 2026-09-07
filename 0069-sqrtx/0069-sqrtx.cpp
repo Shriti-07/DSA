8b0de5307b3e27417e26c4bae98a1c3090669c29
Shriti-07 <shriti.tech07@gmail.com>
2026-09-07 23:25:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        if(x==0 || x==1){
+            return x;
+        }
+        int lower=0;
+        int upper=x/2;
+        int ans=0;
+        while(lower<=upper){
+            long long mid=lower+(upper-lower)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid>x){
+                upper=mid-1;
+            }
+            else{
+                ans=mid;
+                lower=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+//time complexity:O(√x)
+/* class Solution {
+public:
+    int mySqrt(int x) {
         for(long long i=1;;i++){
             if(i*i==x){
                 return i;
@@ -10,4 +36,4 @@ public:
             }
         }
     }
-};
+}; */
