ef1675eb2d709ceeb99f9117acb8058570aa6215
Shriti-07 <shriti.tech07@gmail.com>
2026-09-08 10:10:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (23.39%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+        int i=0;
+        string character="";
+        while(i<s.length()){
+            if(s==goal){
+                return true;
+            }
+            s=s+s[0];
+            s.erase(0,1);
+            i++;
+        }
+        return false;
+    }
+};
