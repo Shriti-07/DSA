bbd5af4c5c63ecf2e832330175f6ad5758dad928
Shriti-07 <shriti.tech07@gmail.com>
2026-09-09 21:09:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int countCommas(int n) {
+        if(n<1000) return 0;
+        long long lower=1000;
+        long long upper=9999;
+        long long total=0;
+        long long commasPerNum=1;
+        while(lower<=n){
+            long long count = min(upper, (long long)n) - lower + 1;
+            total += count*commasPerNum;
+            lower*=10;
+            upper=((upper+1)*10)-1;
+        }
+        return total;
+    }
+};
+
+/* class Solution {
+public:
+    int countCommas(int n) {
         n = abs(n); 
         int count = 0;
         int commas=0;
@@ -17,4 +35,4 @@ public:
         }
     return commas; 
     }
-};
+}; */
