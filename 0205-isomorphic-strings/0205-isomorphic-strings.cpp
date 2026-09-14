07309bde7c850aef312890cf9c71dffa4cc20ac4
Shriti-07 <shriti.tech07@gmail.com>
2026-09-14 09:40:16 +0530

    Time: 1 ms (52.88%), Space: 9.2 MB (82.88%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if(s.length() != t.length()) return false;
+        int map_s[256]={0};
+        int map_t[256]={0};
+
+        for(int i=0;i<s.length();i++){
+            unsigned char char_s=s[i];
+            unsigned char char_t=t[i];
+
+            if(map_s[char_s] != map_t[char_t]){
+                return false;
+            }
+
+            map_s[char_s]=i+1;
+            map_t[char_t]=i+1;
+        }
+        return true;
+    }
+};
+
+//Not optimal
+/* class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()) return false;
         int n=s.length();
         unordered_map<char , char> mp1;
         unordered_map<char , char> mp2;
@@ -21,4 +44,4 @@ public:
         }
         return true;
     }
-};
+}; */
