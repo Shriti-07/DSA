01568eeb8aa39b64b75162b6dad8d7dd7d4711fc
Shriti-07 <shriti.tech07@gmail.com>
2026-09-26 20:18:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (26.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,7 +8,21 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
+
+/* class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         ListNode* temp=head;
@@ -27,4 +41,4 @@ public:
         }
         return head;
     }
-};
+}; */
